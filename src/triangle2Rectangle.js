0f82d6b463789e51e52128350fe58869af82c621
Brooks Mershon <mershon.brooks@gmail.com>
2016-03-16 22:20:58 -0400

    license

--- src/triangle2Rectangle.js
+++ src/triangle2Rectangle.js
@@ -14,5 +14,6 @@ export default function(T) {
     }
   }
 
-  return maxAngle;
+  // TODO
+  return null;
 };
--- test/triangle2Rectangle-test.js
+++ test/triangle2Rectangle-test.js
@@ -6,6 +6,6 @@ require("./inDelta.js"); // add functionality to test suite
 
 tape("test cutting triangle into canonical rectangle", function(test) {
   let a = [0, 0], b = [1, -1], c = [0, 1];
-  test.equal(cut.triangle2Rectangle([a, b, c]), 3*Math.PI/4);
+  // TODO
   test.end();
 });
