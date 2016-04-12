a1e6efbaefafb1c58e65cbf750a13c970916de97
Brooks Mershon <mershon.brooks@gmail.com>
2016-04-12 15:27:02 -0400

    cutCollection added

---
+++ src/cutCollection.js
@@ -0,0 +1,7 @@
+import {default as intersect} from "./intersect";
+
+// Return array of polygons resulting from cutting
+// convex polygons in the collection by segment ab
+export default function(a, b, collection) {
+  // TODO
+};
