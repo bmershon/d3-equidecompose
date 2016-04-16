66c9ff27eac71c7727ef0136a4a7c47b899ca59f
Brooks Mershon <mershon.brooks@gmail.com>
2016-04-16 16:23:35 -0400

    rectangle2Square checkpoint

--- src/multiplyMatVec.js
+++ src/multiplyMatVec.js
@@ -4,7 +4,7 @@ import {default as dot} from "./dot";
 export default function(A, b) {
   var res = [];
   for(let i = 0; i < A.length; i++){
-    res[i] = dot(row(A,i), b);
+    res[i] = dot(row(A, i), b);
   }
   return res;
 }
--- src/rectangle2Square.js
+++ src/rectangle2Square.js
@@ -1,37 +1,75 @@
 import {default as sub} from "./sub";
 import {default as length} from "./length";
 import {default as normalize} from "./normalize";
+import {default as intersect} from "./intersect";
 import {default as scale} from "./scale";
 import {default as add} from "./add";
-import {polygonArea} from "d3-polygon";
+import {polygonArea, polygonCentroid, polygonContains} from "d3-polygon";
 import {default as cutCollection} from "./cutCollection";
 
 // Takes in array of polygons forming a canonical rectangle
 export default function(collection) {
   var dist = [],
-      A, B, C, D, M1, M2,
+      A, B, C, D, E, F, G, H, J, K, // follows Tralie's labeling
+      AFGD, KCF,
       area, s,
       rectangle = collection.rectangle, // bounding rectangle
       square,
       polygons = [],
-      epsilon = 1e-4;
-
-  A = rectangle[0]; // CCW: ABCD
-  B = rectangle[1];
-  C = rectangle[2];
-  D = rectangle[3];
+      ε = 1e-2;
 
   area = Math.abs(polygonArea(rectangle));
   s = Math.sqrt(area); // square side length
 
-  // assumes escalator conditions hold
-  M1 = add(A, scale(s, normalize(sub(D, A))));
-  M2 = B;
-  console.debug(M1, M2, collection[0])
-  square = [M1, M2];
+  // TODO: Trim collection of polygons (rectangle)
+  // such that width < 2 * square dimension
+  // update collection.rectangle accordingly
 
-  polygons = cutCollection(M1, M2, collection);
+  // assumes escalator conditions hold
+  B = rectangle[0]; // rectangle CCW order
+  E = rectangle[3];
+  F = rectangle[1];
+  G = rectangle[2];
+
+  A = add(B, scale(s, normalize(sub(E, B))));
+  C = add(B, scale(s, normalize(sub(F, B))));
+  D = add(A, sub(C, B));
+  J = intersect(A, F, E, G);
+  K = intersect(A, F, D, C)
+  KCF = [K, C, F]; // used to locate elevator pieces
+  AFGD = [A, F, G, D];
+
+  // epsilon corrections to ensure intersection
+  polygons = cutCollection(D, add(C, scale(ε, sub(C, D))), collection);
+  polygons = cutCollection(A, F, polygons);
+
+  // slide new polygons using elevator method
+  polygons.forEach(function(d) {
+    var centroid, T;
+
+    centroid = polygonCentroid(d);
+
+    if (polygonContains(KCF, centroid)) {
+      T = sub(A, K);
+      polygonTranslate(d, T);
+      d.transform = {translate: scale(-1, T)};
+    } else if (polygonContains(AFGD, centroid)) {
+      T = sub(A, J);
+      polygonTranslate(d, T);
+      d.transform = {translate: scale(-1, T)};
+    }
+  });
+
+  square = [A, B, C, D];
   polygons.square = square;
 
   return polygons;
 };
+
+// keep polygon properties, update vertices
+function polygonTranslate(polygon, T) {
+  for (let i = 0; i < polygon.length; i++) {
+    let v = polygon[i];
+    polygon[i] = [v[0] + T[0], v[1] + T[1]];
+  }
+}
