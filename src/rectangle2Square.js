0268e4d6f953dd46a83f4e02a515a5614562a83d
Brooks Mershon <mershon.brooks@gmail.com>
2016-04-23 21:54:50 -0400

    make triangle 2 square work

--- src/rectangle2Square.js
+++ src/rectangle2Square.js
@@ -16,7 +16,7 @@ export default function(collection) {
       area, s,
       rectangle = collection.rectangle, // bounding rectangle
       square,
-      p, v, q, w,
+      slide,
       l = Infinity,
       polygons = [];
 
@@ -24,9 +24,9 @@ export default function(collection) {
   s = Math.sqrt(area); // square side length
 
   // assumes escalator conditions hold
-  B = rectangle[0]; // rectangle CCW order
-  E = rectangle[3];
-  F = rectangle[1];
+  B = rectangle[0]; // an invariant throughout
+  E = rectangle[3]; // need reference
+  F = rectangle[1]; // need reference
   G = rectangle[2];
 
   // the square defined by [A, B, C, D]
@@ -38,7 +38,9 @@ export default function(collection) {
 
   // halving the canonical rectangle for the escalator method
   while (l > 2 * s) {
-    let a, b, left, halved, slideLeft, slideUp;
+    let a, b, left, halved, slideLeft, slideUp,
+        E_Polygon = [], E_Vertex = [],
+        F_Polygon = [], F_Vertex = [];
 
     a = add(A, scale(0.5, sub(F, B))); 
     b = add(B, scale(0.5, sub(F, B)));
@@ -49,50 +51,58 @@ export default function(collection) {
     // "overshoot" cut segment endpoints to ensure intersection
     halved = cutCollection(a, add(b, sub(C, D)), collection);
 
-    slideUp = sub(G, F);
+    slideUp = sub(E, B);
     slideLeft = sub(B, b);
 
+    // translate polgons resulting from the cut (i.e., "stacking the two halves")
     halved.forEach(function(d, j) {
       var centroid, T;
       centroid = polygonCentroid(d);
 
       // update exact references to vertices used in exact intersections
       d.forEach(function(V, k) { // scan vertices
-        if (Object.is(F, V)) {
-          p = j;
-          v = k;
-        } else if (Object.is(G, V)) {
-          q = j;
-          w = k;
+        if (Object.is(E, V)) {
+          E_Polygon.push(j);
+          E_Vertex.push(k);
+        } else if (Object.is(F, V)) {
+          F_Polygon.push(j);
+          F_Vertex.push(k);
         }
       });
 
       if (polygonContains(left, centroid)) { // slide "up"
         d.translate(slideUp).transforms.push({translate: scale(-1, slideUp)}); // undo translate
       } else { // slide "left"
-        d.translate(slideLeft).transforms.push({translate: scale(-1, slideUp)}); // undo translate
+        d.translate(slideLeft).transforms.push({translate: scale(-1, slideLeft)}); // undo translate
       }
+
     });
 
-    F = halved[p][v]; // updated position after transform
-    G = halved[q][w];
+  E = halved[E_Polygon[0]][E_Vertex[0]];
+  F = halved[F_Polygon[0]][F_Vertex[0]];
 
+  // make all vertices at F share the same reference (to ensure intersection)
+  for (let i = 1, n = F_Vertex.length; i < n; i++) {
+    F = halved[F_Polygon[i]][F_Vertex[i]] = halved[F_Polygon[i-1]][F_Vertex[i-1]]; 
+  }
     collection = halved;
   }
-
+  
   polygons = collection;
 
+  // Following "elevator method" assumes rectangle length < 2 x square height
+  G = add(F, sub(E, B));
   J = intersect(A, F, E, G);
   K = intersect(A, F, D, C)
   KCF = [K, C, F]; // used to locate elevator pieces
   AFGD = [A, F, G, D];
 
-  polygons = cutCollection(D, add(C, scale(1, sub(C, D))), collection);
   polygons = cutCollection(A, F, polygons);
+  polygons = cutCollection(D, add(C, scale(1, sub(C, D))), polygons);
 
   // slide new polygons using elevator method
   polygons.forEach(function(d) {
-    var centroid, T, P;
+    var centroid, T;
 
     centroid = polygonCentroid(d);
 
