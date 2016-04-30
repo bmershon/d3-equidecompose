391990f986c8e57d4a497bb45073ab96e4315a7f
Joy Patel <joypatel0123@gmail.com>
2016-04-30 04:00:15 -0400

    Added comments.

--- src/orientCounterClockwise.js
+++ src/orientCounterClockwise.js
@@ -1,13 +1,11 @@
 import polygon from "./polygon";
 
+// got from here: http://gamedev.stackexchange.com/questions/13229/sorting-array-of-points-in-clockwise-order
+// orients points in counter clockwise direction
+
 export default function(points){
 	var poly = polygon(points);
 	var c = poly.centroid();
-	/*var a = [];
-	for(let i = 0; i < points.length; i++){
-		var p = points[i];
-		a.push(Math.atan2(p[1] - c[1],p[0] - c[0]));
-	}*/
 	points.sort(function(a,b){
 		return  Math.atan2(b[1] - c[1],b[0] - c[0]) - Math.atan2(a[1] - c[1],a[0] - c[0]);
 	})
