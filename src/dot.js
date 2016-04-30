183b0f26d72ae44f65eb03106b11d2ca410f6926
Brooks Mershon <mershon.brooks@gmail.com>
2016-04-30 15:25:22 -0400

    img

--- src/dot.js
+++ src/dot.js
@@ -3,13 +3,16 @@ Computes the dot product between 2 vectors.
 */
 
 export default function(a, b) {
-  if(a.length != b.length){
+  var res = 0.0;
+
+  if (a.length != b.length){
     throw new Error("Invalid dimensions for dot product.");
     return null;
   }
-  var res = 0.0;
-  for(let i = 0; i < a.length; i++){
-    res += a[i]*b[i]; 
+
+  for (let i = 0; i < a.length; i++) {
+    res += a[i] * b[i]; 
   }
+
   return res;
 }
