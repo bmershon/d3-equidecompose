df4f55c648d3c72b779c3b65a9c2bb3763ffab19
Brooks Mershon <mershon.brooks@gmail.com>
2016-05-21 19:51:13 -0400

    counter clockwise sorting

--- src/counterClockwise.js
+++ src/counterClockwise.js
@@ -1,12 +1,9 @@
+import right from "./right";
 import polygon from "./polygon";
 
 // In-place sort points in counter-clockwise order.
 export default function(P){
-  var c = P.centroid();
-
-  P.sort(function(a, b) {
-    return Math.atan2(b[1] - c[1], b[0] - c[0]) - Math.atan2(a[1] - c[1], a[0] - c[0]);
-  });
-
+  var C = P.centroid();
+  if (!right(C, P.slice(0, 2))) P.reverse();
   return P;
 }
