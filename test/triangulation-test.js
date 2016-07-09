71013396a6f6d64db44357866e5960722674cb51
Brooks Mershon <mershon.brooks@gmail.com>
2016-07-09 15:02:41 -0600

    checkpoint

--- test/triangulation-test.js
+++
@@ -1,9 +0,0 @@
-var tape = require("tape"),
-    dev = require("../build/dev.js");
-
-tape("triangulation of a concave polygon", function(test) {
-  var polygon = [[50, 50], [150, 400], [400, 500], [200, 100], [100, 20]];
-  console.log(dev.triangulate(polygon));
-  test.equal(dev.triangulate(polygon), null);
-  test.end();
-});
