677a09c7fca43a3ab2d8c479cd6dd0057772a3ae
Brooks Mershon <mershon.brooks@gmail.com>
2016-08-18 23:22:22 -0600

    remove unused API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-export {equidecompose} from "./src/equidecompose/equidecompose";
-export {equidecomposeMesh} from "./src/equidecompose/equidecompose";
-export {equidecomposeTopology} from "./src/equidecompose/equidecompose";
+export {
+  equidecompose,
+  equidecomposeMesh
+} from "./src/equidecompose/equidecompose";
--- src/equidecompose/equidecompose.js
+++ src/equidecompose/equidecompose.js
@@ -1,21 +1,10 @@
 import {default as decomposition} from "./decomposition";
 import {default as triangulate} from "../geom/triangulate.js";
 
-// Takes in two simple polygons, triangulates each,
-// scales the subject as necessary, and returns 
-// a decomposition object.
 export function equidecompose(source, subject) {
   return decomposition(triangulate(source), triangulate(subject));  
 }
 
-// Takes in two collections of triangles (triangulations), 
-// scales the subject as necessary, and
-// returns a decomposition object.
 export function equidecomposeMesh(source, subject) {
-  return decomposition(source, subject);
-}
-
-export function equidecomposeTopology(source, subject) {
-  // TODO 
   return decomposition(source, subject);
 }
